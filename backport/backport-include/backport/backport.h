ffa4ddf7d34b3ad5aae528f00380efdd80ac0b24
S A Muqthyar Ahmed <syed.abdul.muqthyar.ahmed@intel.com>
2024-10-30 20:40:01 +0530

    Clean-up: Remove unnecessary files for xe driver backport
    Remove files which are not needed for Xe backports from folders
    Backport-include headers
    compat files
    defconfigs
    patches
    
    Reviewed-by: Ayaz A Siddiqui <ayaz.siddiqui@intel.com>
    Signed-off-by: S A Muqthyar Ahmed <syed.abdul.muqthyar.ahmed@intel.com>

--- backport/backport-include/backport/backport.h
+++ backport/backport-include/backport/backport.h
@@ -8,9 +8,6 @@
 
 #ifndef __ASSEMBLY__
 #define LINUX_BACKPORT(__sym) backport_ ##__sym
-#ifndef CONFIG_BACKPORT_INTEGRATE
-#include <backport/checks.h>
-#endif
 #endif
 
 #endif /* __BACKPORT_H */
--- patches/0096-ethtool-supported_coalesce_params.cocci
+++
@@ -1,9 +0,0 @@
-@r@
-identifier s,expr;
-@@
-
-struct ethtool_ops s = {
-+#if LINUX_VERSION_IS_GEQ(5,7,0)
-	.supported_coalesce_params = expr,
-+#endif
-};

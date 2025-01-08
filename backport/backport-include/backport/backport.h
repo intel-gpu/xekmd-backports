1a4fa932da4c475086cc2ebdf0a90fa3d4701bcb
S A Muqthyar Ahmed <syed.abdul.muqthyar.ahmed@intel.com>
2025-01-09 01:12:22 +0530

    Makefile: convert assign_str prototype check using parser
    Fix current version control for assign_str function by implementing a
    parser script called before build and dynamically generate a macro in a new header
    based on prototype.
    
    Reviewed-by: Ayaz A Siddiqui <ayaz.siddiqui@intel.com>
    Signed-off-by: S A Muqthyar Ahmed <syed.abdul.muqthyar.ahmed@intel.com>

--- backport/backport-include/backport/backport.h
+++ backport/backport-include/backport/backport.h
@@ -6,14 +6,10 @@
 #endif
 #include <linux/kconfig.h>
 #include <backport/backport_auto.h>
+#include <backport/automacro.h>
 
 #ifndef __ASSEMBLY__
 #define LINUX_BACKPORT(__sym) backport_ ##__sym
-#endif
-
-/* FIXME: Auto tools conversion is required for assign_str */
-#if LINUX_VERSION_IS_LESS(6,10,0)
-#define BPM_ASSIGN_STR_SECOND_ARG_PRESENT
 #endif
 
 #endif /* __BACKPORT_H */

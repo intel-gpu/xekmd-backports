dc71acc4592ffb4aa4e610816a26d6bf655d47eb
Rachapalli Bandi Jagadeesh <jagadeesh.rachapalli.bandi@intel.com>
2025-10-26 23:24:05 +0530

    autotools/m4: Use cancel_work_sync instead of disable_work_sync
    cancel_work_sync() api has been renamed as disable_work_sync from 6.10 kernel.
    Hence observing below error for 6.6 kernel.
    To fix this error, replaced disable_work_sync() with cancel_work_sync()
    for below 6.10 kernels.
    
    Error:
    -----------------------------------------------------------------------
    error: implicit declaration of function ‘disable_work_sync’
    -----------------------------------------------------------------------
    
    Reference:
            86898fa6b8cd
            workqueue: Implement disable/enable for (delayed) work items
    
    Reviewed-by: S A Muqthyar Ahmed <syed.abdul.muqthyar.ahmed@intel.com>
    Signed-off-by: Rachapalli Bandi Jagadeesh <jagadeesh.rachapalli.bandi@intel.com>

--- backport/backport-include/linux/workqueue.h
+++ backport/backport-include/linux/workqueue.h
@@ -1,4 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * workqueue.h --- work queue handling for Linux.
+ */
 
 #ifndef __BACKPORT_LINUX_WORKQUEUE_H
 #define __BACKPORT_LINUX_WORKQUEUE_H
@@ -8,6 +11,10 @@
 
 #define alloc_ordered_workqueue_lockdep_map(fmt, flags, lockdep_map, ...) \
 		alloc_ordered_workqueue(fmt, flags)
+#endif
+
+#ifdef BPM_DISBALE_WORK_SYNC_NOT_PRESENT
+#define disable_work_sync cancel_work_sync
 #endif
 
 #endif /* __BACKPORT_LINUX_WORKQUEUE_H */

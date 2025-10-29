eca1f5d5ed4d6717dade1affad9d9b0517664ef8
Rachapalli Bandi Jagadeesh <jagadeesh.rachapalli.bandi@intel.com>
2025-10-29 17:59:00 +0530

    Backport DEFINE_SHOW_STORE_ATTRIBUTE() macro
    DEFINE_SHOW_STORE_ATTRIBUTE() macro has been introduced from 6.7 kernel.
    Hence observing below error for 6.6 kernel.
    To fix this error, backported DEFINE_SHOW_STORE_ATTRIBUTE() macro for
    below 6.6 kernels.
    
    Error:
    -------------------------------------------------------------------------------
    error: type defaults to ‘int’ in declaration of ‘DEFINE_SHOW_STORE_ATTRIBUTE’
    error: ‘force_reset_fops’ undeclared
    error: ‘force_reset_sync_fops’ undeclared
    warning: ‘force_reset_sync_show’ defined but not used
    warning: ‘force_reset_sync_write’ defined but not used
    warning: ‘force_reset_show’ defined but not used
    warning: ‘force_reset_write’ defined but not used
    -------------------------------------------------------------------------------
    
    Reference:
    	9cba82bba500 seq_file: add helper macro to define attribute for rw file
    
    Reviewed-by: S A Muqthyar Ahmed <syed.abdul.muqthyar.ahmed@intel.com>
    Signed-off-by: Rachapalli Bandi Jagadeesh <jagadeesh.rachapalli.bandi@intel.com>

--- backport/backport-include/linux/seq_file.h
+++ backport/backport-include/linux/seq_file.h
@@ -41,4 +41,23 @@ static const struct file_operations __name ## _fops = {			\
 }
 #endif /* DEFINE_SEQ_ATTRIBUTE */
 
+#ifndef DEFINE_SHOW_STORE_ATTRIBUTE
+
+#define DEFINE_SHOW_STORE_ATTRIBUTE(__name)				\
+static int __name ## _open(struct inode *inode, struct file *file)	\
+{									\
+	return single_open(file, __name ## _show, inode->i_private);	\
+}									\
+									\
+static const struct file_operations __name ## _fops = {			\
+	.owner		= THIS_MODULE,					\
+	.open		= __name ## _open,				\
+	.read		= seq_read,					\
+	.write		= __name ## _write,				\
+	.llseek		= seq_lseek,					\
+	.release	= single_release,				\
+}
+
+#endif
+
 #endif /* __BACKPORT_SEQ_FILE_H */

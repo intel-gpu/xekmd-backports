d41d816659dbd584f2b193fa08acbcc5ca30e0f5
Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>
2025-10-30 00:10:29 +0530

    XE-KMD Backport-Release: xebr-v6.14.11-250909.17.1.0
    Generation Information:
    XeKMD-Backport-Tag: xe-v6.14.11-250909.17
    Backports Tag: xeb_v6.14.11_250909.17.1
    Backports Branch: oot-backport/main
    Backports Head: 40cf10b
    
    Reviewed-by: S A Muqthyar Ahmed <syed.abdul.muqthyar.ahmed@intel.com>
    Signed-off-by: Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>

--- src/backport-include/linux/export.h
+++ src/backport-include/linux/export.h
@@ -5,6 +5,11 @@
 
 #include_next <linux/export.h>
 
+#ifdef BPM_MODULE_IMPORT_TO_STRING_LITERAL_PRESENT
+#undef EXPORT_SYMBOL_NS_GPL
+#define EXPORT_SYMBOL_NS_GPL(sym, ns) __EXPORT_SYMBOL(sym, "GPL", ns)
+#endif
+
 #ifndef EXPORT_SYMBOL_NS_GPL
 #define EXPORT_SYMBOL_NS_GPL(sym, ns) EXPORT_SYMBOL_GPL(sym)
 #endif

0280facbfbba8048f8a5499fc6d75e96a3094660
SYS SEOS AUTOBACKPORT <sys_seos_autobackport@intel.com>
2026-02-06 11:24:26 +0530

    XE-KMD Backport-Release: xebr-v6.17.13-260130.26.0.0
    Generation Information:
    XeKMD-Backport-Tag: v6.17.13-260130.26
    Backports Tag: xeb_v6.17.13-260130.26.0
    Backports Branch: oot-backport/main
    Backports Head: a9de4c501
    
    Signed-off-by: SYS SEOS AUTOBACKPORT <sys_seos_autobackport@intel.com>

---
+++ src/backport-include/backport/automacro.h
@@ -0,0 +1,10 @@
+#ifndef _BACKPORTED_AUTO_MACRO_H
+#define _BACKPORTED_AUTO_MACRO_H
+/*
+ * Automatically generated file, don't edit!
+ * Changes will be overwritten
+ */
+
+#define BPM_ASSIGN_STR_SECOND_ARG_PRESENT
+
+#endif /* _BACKPORTED_AUTO_MACRO_H */

54769fcae9109083ab103304cccf650bad59e33b
Rachapalli Bandi Jagadeesh <jagadeesh.rachapalli.bandi@intel.com>
2026-04-21 18:49:46 +0530

    Fix class_create() owner arg issue
    Errors:
    -----------------------------------------------------------------------------
    error: macro "class_create" requires 2 arguments, but only 1 given
    error: ‘class_create’ undeclared (first use in this function)
    -----------------------------------------------------------------------------
    
    Signed-off-by: Rachapalli Bandi Jagadeesh <jagadeesh.rachapalli.bandi@intel.com>

--- backport/backport-include/linux/device.h
+++ backport/backport-include/linux/device.h
@@ -12,4 +12,28 @@
 	struct device_attribute dev_attr_##_name = __ATTR_RO_MODE(_name, 0400)
 #endif
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 4, 0)
+#ifdef class_create
+#undef class_create
+#endif
+/**
+ * class_create - create a struct class structure
+ * @owner: pointer to the module that is to "own" this struct class
+ * @name: pointer to a string for the name of this class.
+ *
+ * This is used to create a struct class pointer that can then be used
+ * in calls to device_create().
+ *
+ * Returns &struct class pointer on success, or ERR_PTR() on error.
+ *
+ * Note, the pointer created here is to be destroyed when finished by
+ * making a call to class_destroy().
+ */
+#define class_create(name)				\
+({							\
+	static struct lock_class_key __key;		\
+	__class_create(THIS_MODULE, name, &__key);	\
+})
+#endif
+
 #endif /* __BACKPORT_DEVICE_H_ */

21f602ba61309a8d3172b1e879f8967e9f5e2e67
Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>
2026-05-12 19:36:00 +0530

    backport-include/mm: manage vm_flags_set/clear/mod helpers for pre-6.3 kernels
    vm_flags_set(), vm_flags_clear() and vm_flags_mod() were introduced in
    linux v6.3 as helpers to modify vma->vm_flags. On base kernel 5.15 these
    functions do not exist, causing implicit declaration errors. Add an m4
    detection probe and provide inline macro fallbacks in backport-include.
    
    Error:
    -------------------------------------------------------------------------------
    implicit declaration of function 'vm_flags_set'
    -------------------------------------------------------------------------------
    
    Reference:
      c1e3f1c611e6
      mm: introduce vm_flags_set/vm_flags_clear
    
    Signed-off-by: Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>

--- backport/backport-include/linux/mm.h
+++ backport/backport-include/linux/mm.h
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: MIT */
+/* SPDX-License-Identifier: GPL-2.0 */
 #ifndef __BACKPORT_LINUX_MM_H
 #define __BACKPORT_LINUX_MM_H
 
@@ -9,4 +9,16 @@ extern int access_remote_vm(struct mm_struct *mm, unsigned long addr,
         void *buf, int len, unsigned int gup_flags);
 #endif
 
-#endif /* __BACKPORT_LINUX_MM_H__ */
+#ifdef BPM_VM_FLAGS_SET_NOT_PRESENT
+#define vm_flags_set(vma, flags)	((vma)->vm_flags |= (flags))
+
+#define vm_flags_clear(vma, flags)	((vma)->vm_flags &= ~(flags))
+
+#define vm_flags_mod(vma, set, clear)	\
+	do {				\
+		vm_flags_set((vma), (set));	\
+		vm_flags_clear((vma), (clear));	\
+	} while (0)
+#endif
+
+#endif /* __BACKPORT_LINUX_MM_H */

8f8552022a27d3fb6d8d3e20fadf13877fb57145
Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>
2026-05-12 19:09:22 +0530

    autotools/m4: manage dma_resv_for_each_fence() api availability
    dma_resv_for_each_fence() was introduced in Linux 5.16.
    Add autotools detection and use a backport fallback path for older
    kernels (such as 5.15) where the API is not present.
    
    Error:
    -------------------------------------------------------------------------------
    implicit declaration of function dma_resv_for_each_fence
    -------------------------------------------------------------------------------
    
    Reference:
        5baaac3184ab
        dma-buf: add dma_resv_for_each_fence v3
    
    Signed-off-by: Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>

--- backport/backport-include/linux/dma-resv.h
+++ backport/backport-include/linux/dma-resv.h
@@ -16,4 +16,13 @@ static inline enum dma_resv_usage dma_resv_usage_rw(bool write)
 	return write ? DMA_RESV_USAGE_READ : DMA_RESV_USAGE_WRITE;
 }
 #endif
+
+#ifdef BPM_DMA_RESV_FOR_EACH_FENCE_NOT_PRESENT
+#define dma_resv_for_each_fence(cursor, obj, usage, fence) \
+        for (dma_resv_iter_begin((cursor), (obj), \
+                              (usage) < DMA_RESV_USAGE_BOOKKEEP), \
+             (fence) = dma_resv_iter_first_unlocked((cursor)); \
+             (fence); \
+             (fence) = dma_resv_iter_next_unlocked((cursor)))
+#endif
 #endif

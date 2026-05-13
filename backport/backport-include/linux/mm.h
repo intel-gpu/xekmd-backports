5c0848181122455375a1ae9c3dd3e3bc0a5b78f5
Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>
2026-05-13 09:51:12 +0530

    backport-include: add folio helper functions in mm.h for 5.15 kernel
    ttm_backup uses folio helper APIs that are missing on 5.15, causing
    implicit declaration failures during build.
    
    Fix by adding compat fallbacks in linux/mm.h for missing folio helpers,
    mapping them to equivalent page-based helpers used on older kernels.
    
    Error:
    implicit declaration of function 'folio_put'
    implicit declaration of function 'folio_mark_accessed'
    implicit declaration of function 'folio_lock'
    implicit declaration of function 'folio_mark_dirt
    implicit declaration of function 'folio_mapped'
    implicit declaration of function 'folio_clear_dirty_for_io'
    
    Reference:
        b620f63358cd ("mm: Add folio_put()")
        b5e84594cafb ("mm/writeback: Add folio_mark_dirty()")
        9350f20a070d ("mm/writeback: Add folio_clear_dirty_for_io()")
    
    Signed-off-by: Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>

--- backport/backport-include/linux/mm.h
+++ backport/backport-include/linux/mm.h
@@ -16,4 +16,48 @@
 	} while (0)
 #endif
 
+#ifdef BPM_FOLIO_FILE_PAGE_NOT_PRESENT
+#define folio_file_page(folio, idx) ((struct page *)(folio))
+#endif
+
+#ifdef BPM_FOLIO_PUT_NOT_PRESENT
+#define folio_put(folio) put_page((struct page *)(folio))
+#endif
+
+#ifdef BPM_FOLIO_MARK_ACCESSED_NOT_PRESENT
+#define folio_mark_accessed(folio) mark_page_accessed((struct page *)(folio))
+#endif
+
+#ifdef BPM_FOLIO_LOCK_NOT_PRESENT
+#define folio_lock(folio) lock_page((struct page *)(folio))
+#endif
+
+#ifdef BPM_FOLIO_UNLOCK_NOT_PRESENT
+#define folio_unlock(folio) unlock_page((struct page *)(folio))
+#endif
+
+#ifdef BPM_FOLIO_MARK_DIRTY_NOT_PRESENT
+#define folio_mark_dirty(folio) set_page_dirty((struct page *)(folio))
+#endif
+
+#ifdef BPM_FOLIO_MAPPED_NOT_PRESENT
+#define folio_mapped(folio) page_mapped((struct page *)(folio))
+#endif
+
+#ifdef BPM_FOLIO_CLEAR_DIRTY_FOR_IO_NOT_PRESENT
+#define folio_clear_dirty_for_io(folio) clear_page_dirty_for_io((struct page *)(folio))
+#endif
+
+#ifdef BPM_FOLIO_SET_RECLAIM_NOT_PRESENT
+#define folio_set_reclaim(folio) SetPageReclaim((struct page *)(folio))
+#endif
+
+#ifdef BPM_FOLIO_CLEAR_RECLAIM_NOT_PRESENT
+#define folio_clear_reclaim(folio) ClearPageReclaim((struct page *)(folio))
+#endif
+
+#ifdef BPM_FOLIO_TEST_WRITEBACK_NOT_PRESENT
+#define folio_test_writeback(folio) PageWriteback((struct page *)(folio))
+#endif
+
 #endif /* __BACKPORT_LINUX_MM_H */

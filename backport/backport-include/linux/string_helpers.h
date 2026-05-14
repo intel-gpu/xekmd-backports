0c09ea7e89ba521f77e5cde7f20c39102a5f0ee8
Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>
2026-05-14 12:32:30 +0530

    backport-include: backport string_helpers.h
    From 5.16 kernel string.h is included in string_helpers.h.
    
    Backport string_helpers.h header and include string.h file
    to resolve below error.
    
    Error:
    implicit declaration of function strlen
    
    Reference:
        bfb3ba32061d
        include/linux/string_helpers.h: add linux/string.h for strlen()
    
    Signed-off-by: Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>

---
+++ backport/backport-include/linux/string_helpers.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __BACKPORT_LINUX_STRING_HELPERS_H
+#define __BACKPORT_LINUX_STRING_HELPERS_H
+
+#include <linux/string.h>
+
+#include_next <linux/string_helpers.h>
+
+#endif /* __BACKPORT_LINUX_STRING_HELPERS_H */

dcf82ec9b91fd4143879932c6ec11f7f098ca218
Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>
2026-05-14 16:00:42 +0530

    autotools/m4: manage drmm_mutex_init() api availability
    The API was introduced in v5.19 kernel.
    
    On Linux 5.15, Xe fails with implicit declaration errors for drmm_mutex_init()
    because the kernel's drm_managed.h does not expose it consistently.
    
    Resolved by adding an m4 file to detect availability and implementing
    drmm_mutex_init() in the compat layer when absent.
    
    Error:
    implicit declaration of function 'drmm_mutex_init'
    
    Reference:
    e13f13e039dc (drm: Add DRM-managed mutex_init())
    
    Signed-off-by: Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>

--- backport/compat/drm_managed.c
+++ backport/compat/drm_managed.c
@@ -6,6 +6,7 @@
  */
 
 #include <drm/drm_managed.h>
+#include <linux/mutex.h>
 
 #ifdef BPM_DRMM_WORKQUEUE_RELEASE_NOT_PRESENT
 void __drmm_workqueue_release(struct drm_device *device, void *res)
@@ -15,4 +16,32 @@ void __drmm_workqueue_release(struct drm_device *device, void *res)
 	destroy_workqueue(wq);
 }
 EXPORT_SYMBOL(__drmm_workqueue_release);
+#endif
+
+#ifdef BPM_DRMM_MUTEX_INIT_NOT_PRESENT
+static void drmm_mutex_release(struct drm_device *dev, void *res)
+{
+        struct mutex *lock = res;
+
+        mutex_destroy(lock);
+}
+
+/**
+ * drmm_mutex_init - &drm_device-managed mutex_init()
+ * @dev: DRM device
+ * @lock: lock to be initialized
+ *
+ * Returns:
+ * 0 on success, or a negative errno code otherwise.
+ *
+ * This is a &drm_device-managed version of mutex_init(). The initialized
+ * lock is automatically destroyed on the final drm_dev_put().
+ */
+int drmm_mutex_init(struct drm_device *dev, struct mutex *lock)
+{
+        mutex_init(lock);
+
+        return drmm_add_action_or_reset(dev, drmm_mutex_release, lock);
+}
+EXPORT_SYMBOL(drmm_mutex_init);
 #endif

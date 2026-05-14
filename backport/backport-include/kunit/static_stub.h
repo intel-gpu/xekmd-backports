a46fe71f699a928632a89971397a7c1b8f6c6d3a
Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>
2026-05-14 15:24:20 +0530

    backport-include: fix build without kunit static-stub header
    On 5.15, Xe build fails because kunit/static_stub.h is missing.
    
    Resolved by adding a compatibility fallback so builds continue
    without the header, while preserving normal behavior when it is present.
    
    Error:
    fatal error: kunit/static_stub.h: No such file or directory
    
    Reference:
    e047c5eaa763 (kunit: Expose 'static stub' API to redirect functions)
    
    Signed-off-by: Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>

---
+++ backport/backport-include/kunit/static_stub.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * KUnit function redirection (static stubbing) API.
+ *
+ * Copyright (C) 2022, Google LLC.
+ * Author: David Gow <davidgow@google.com>
+ */
+
+#ifndef _KUNIT_STATIC_STUB_H
+#define _KUNIT_STATIC_STUB_H
+
+#ifdef HAVE_KUNIT_STATIC_STUB_H
+#include_next <kunit/static_stub.h>
+#endif
+
+#ifndef KUNIT_STATIC_STUB_REDIRECT
+#define KUNIT_STATIC_STUB_REDIRECT(real_fn_name, args...) do {} while (0)
+#endif
+
+#endif /* _KUNIT_STATIC_STUB_H */
+

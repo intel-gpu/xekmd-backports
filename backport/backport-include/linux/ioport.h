2fa158ef433acdbbc09f976ced999a37061f06f5
Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>
2026-05-15 19:53:45 +0530

    backport/ioport: make DEFINE_RES_NAMED usable in assignments
    5.15 builds fail in xe_i2c_register_adapter() with
    "expected expression before '{'" because DEFINE_RES_NAMED expands to a
    brace initializer and is used on the right-hand side of an assignment.
    
    Redefine DEFINE_RES_NAMED in the ioport backport wrapper as a compound
    literal so it works in both declaration and assignment contexts.
    
    Signed-off-by: Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>

--- backport/backport-include/linux/ioport.h
+++ backport/backport-include/linux/ioport.h
@@ -6,4 +6,13 @@
 #define IORESOURCE_REG		0x00000300
 #endif
 
+#undef DEFINE_RES_NAMED
+#define DEFINE_RES_NAMED(_start, _size, _name, _flags)			\
+	(struct resource) {						\
+		.start = (_start),					\
+		.end   = (_start) + (_size) - 1,			\
+		.name  = (_name),					\
+		.flags = (_flags),					\
+		.desc  = IORES_DESC_NONE,				\
+	}
 #endif /* __BACKPORT_LINUX_IOPORT_H */

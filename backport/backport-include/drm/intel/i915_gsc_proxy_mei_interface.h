fc3f1014dd98ebeacda22a44860f1c00a3b003f5
Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>
2026-05-15 11:45:48 +0530

    autotools/m4: manage i915_gsc_proxy_mei_interface.h header for 5.15
    i915_gsc_proxy_mei_interface.h header is not present in 5.15 kernel, use
    nested condition loop for 5.15 kernel.
    
    Error:
    fatal error: drm/intel/i915_gsc_proxy_mei_interface.h: No such file or directory
        7 | #include_next <drm/intel/i915_gsc_proxy_mei_interface.h>
    
    Signed-off-by: Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>

--- backport/backport-include/drm/intel/i915_gsc_proxy_mei_interface.h
+++ backport/backport-include/drm/intel/i915_gsc_proxy_mei_interface.h
@@ -1,10 +1,28 @@
 #ifndef _BACKPORT_I915_GSC_PROXY_MEI_INTERFACE_H_
 #define _BACKPORT_I915_GSC_PROXY_MEI_INTERFACE_H_
 
-#ifdef HAVE_DRM_I915_GSC_PROXY_MEI_INTERFACE_H
+#if defined(HAVE_DRM_INTEL_I915_GSC_PROXY_MEI_INTERFACE_H)
+#include_next <drm/intel/i915_gsc_proxy_mei_interface.h>
+#elif defined(HAVE_DRM_I915_GSC_PROXY_MEI_INTERFACE_H)
 #include <drm/i915_gsc_proxy_mei_interface.h>
 #else
-#include_next <drm/intel/i915_gsc_proxy_mei_interface.h>
+
+#include <linux/types.h>
+
+struct device;
+struct module;
+
+struct i915_gsc_proxy_component_ops {
+	struct module *owner;
+	int (*send)(struct device *dev, const void *buf, size_t size);
+	int (*recv)(struct device *dev, void *buf, size_t size);
+};
+
+struct i915_gsc_proxy_component {
+	struct device *mei_dev;
+	const struct i915_gsc_proxy_component_ops *ops;
+};
+
 #endif
 
 #endif /* _BACKPORT_I915_GSC_PROXY_MEI_INTERFACE_H_ */

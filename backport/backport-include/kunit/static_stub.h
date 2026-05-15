5f0c811fd5843112ccb0efc8877043b192374793
Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>
2026-05-15 20:08:22 +0530

    backport-include: backport kunit_get_current_test() api for 5.15
    kunit_get_current_test() was introduced in 5.16.
    On older kernels there is never an active KUnit test, return NULL.
    
    Error:
    implicit declaration of function ‘kunit_get_current_test’
    
    Signed-off-by: Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>

--- backport/backport-include/kunit/static_stub.h
+++ backport/backport-include/kunit/static_stub.h
@@ -15,5 +15,25 @@
 #define KUNIT_STATIC_STUB_REDIRECT(real_fn_name, args...) do {} while (0)
 #endif
 
+#if LINUX_VERSION_IS_GEQ(5,16,0)
+#include_next <kunit/test.h>
+#else
+#include <linux/stddef.h>
+
+struct kunit;
+
+#ifndef kunit_get_current_test
+static inline struct kunit *kunit_get_current_test(void)
+{
+        return NULL;
+}
+#endif
+
+#ifndef kunit_fail_current_test
+#define kunit_fail_current_test(fmt, ...) do {} while (0)
+#endif
+
+#endif /* < 5.16 */
+
 #endif /* _KUNIT_STATIC_STUB_H */
 

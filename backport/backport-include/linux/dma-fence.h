f8b1b0466a078890f146a6517634c95f43c2b13c
Varun Gupta <varun.gupta@intel.com>
2026-05-19 18:44:09 +0530

    backport: Inline dma_fence_check_and_signal_locked() in dma-fence.h
    v7.0 added dma_fence_check_and_signal_locked() as a small inline-able
    helper that checks DMA_FENCE_FLAG_SIGNALED_BIT and then calls
    dma_fence_signal_locked(). On <v7.0 we previously provided this as an
    out-of-line symbol in compat/dma-fence.c; move it to a static inline
    in backport-include/linux/dma-fence.h so consumers do not depend on a
    compat module symbol.
    
    Drops the now-empty compat/dma-fence.c.
    
    -----------------------------------------------------------------
    error: xe_hw_fence.c:93: implicit declaration of function
    	'dma_fence_check_and_signal_locked'
    -----------------------------------------------------------------
    
    Reference:
    	c891b99d25
    	dma-buf/dma-fence: Add dma_fence_check_and_signal()
    
    Reviewed-by: Syed Abdul Muqthyar Ahmed <syed.abdul.muqthyar.ahmed@intel.com>
    Signed-off-by: Varun Gupta <varun.gupta@intel.com>

--- backport/backport-include/linux/dma-fence.h
+++ backport/backport-include/linux/dma-fence.h
@@ -4,14 +4,19 @@
 #include_next <linux/dma-fence.h>
 
 #ifdef BPM_DMA_FENCE_CHECK_AND_SIGNAL_LOCKED_NOT_PRESENT
-#ifndef dma_fence_test_signaled_flag
-static inline bool dma_fence_test_signaled_flag(struct dma_fence *fence)
+/*
+ * dma_fence_check_and_signal_locked - check if signaled and signal if not
+ * Must be called with fence->lock held.
+ * Returns true if fence was already signaled, false otherwise.
+ */
+static inline bool
+dma_fence_check_and_signal_locked(struct dma_fence *fence)
 {
-	return test_bit(DMA_FENCE_FLAG_SIGNALED_BIT, &fence->flags);
-}
-#endif
+	bool was_signaled = test_bit(DMA_FENCE_FLAG_SIGNALED_BIT, &fence->flags);
 
-bool dma_fence_check_and_signal_locked(struct dma_fence *fence);
+	dma_fence_signal_locked(fence);
+	return was_signaled;
+}
 #endif /* BPM_DMA_FENCE_CHECK_AND_SIGNAL_LOCKED_NOT_PRESENT */
 
 #endif /* __BACKPORT_LINUX_DMA_FENCE_H */

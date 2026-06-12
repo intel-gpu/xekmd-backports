0f51fe79e1a35ff12e417ead2bf30b042288597e
Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>
2026-06-12 14:09:04 +0530

    Fix drm_buddy_block_trim signature mismatch between 5.15 and 6.6
    The drm_buddy_block_trim function has different signatures across kernel
    versions: 5.15 backport uses 4 arguments, while 6.6 native kernel uses
    3 arguments. This caused "too few arguments" compile errors.
    
    Make the prototype conditional based on which kernel version is being
    built, ensuring call sites match the actual function signature.
    
    Signed-off-by: Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>

--- backport/backport-include/drm/drm_buddy.h
+++ backport/backport-include/drm/drm_buddy.h
@@ -8,7 +8,9 @@
 #include_next <drm/drm_buddy.h>
 
 #ifdef BPM_DRM_BUDDY_FREE_LIST_ARG3_NOT_PRESENT
+#if !defined(CPTCFG_BUILD_XE_DRM_BUDDY)
 #define drm_buddy_free_list(x,y,z) drm_buddy_free_list(x,y)
+#endif
 #endif
 
 #endif /* __BACKPORT_DRM_BUDDY_H__ */

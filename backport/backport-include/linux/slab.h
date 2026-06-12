a4da9462f3611002714d9034aca5267f3164d661
Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>
2026-06-12 14:42:21 +0530

    autotools/m4: Add kvrealloc_noprof detection for v6.10+ kernel support
    Kernel v6.10 introduced kvrealloc_noprof() as an alternative to kvrealloc(),
    and made it the primary allocation function. Code using kvrealloc_noprof()
    fails to compile on older kernels (5.15, 6.6) where this function doesn't exist.
    
    Ensures compatibility across supported kernel versions like
    5.15, 6.6, 6.8, and 6.12+.
    
    Reference:
        7bd230a26648
        mm/slab: enable slab allocation tagging for kmalloc and friends
    
    Signed-off-by: Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>

--- backport/backport-include/linux/slab.h
+++ backport/backport-include/linux/slab.h
@@ -173,12 +173,14 @@
 #endif /* BPM_KZALLOC_FLEX_NOT_PRESENT */
 
 #ifdef BPM_KVREALLOC_OLDSIZE_PARAM_PRESENT
+#ifndef BPM_KVREALLOC_NOPROF_PRESENT
 #include <linux/mm.h>
 static inline void *__bp_kvrealloc(const void *p, size_t newsize, gfp_t flags)
 {
 	return kvrealloc(p, 0, newsize, flags);
 }
 #define kvrealloc(p, s, f) __bp_kvrealloc(p, s, f)
+#endif
 #endif
 
 #endif /* _BACKPORT_LINUX_SLAB_H */

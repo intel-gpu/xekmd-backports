ef73172e83b9060dbd1130ad672ceb514d677f1d
Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>
2026-07-02 13:50:11 +0530

    backport-include/slab.h: fix kvrealloc compat on 5.15.198 headers
    The old kvrealloc compat used a fixed 3-arg macro, which can break header
    parsing order on velinux 5.15.198 headers and causes below errors.
    
    Error:
    --------------------------------------------------------------------------------
    implicit declaration of function ‘kvrealloc’
    --------------------------------------------------------------------------------
    
    Use a safer header-only kvrealloc compat form that handles both 3-arg
    and 4-arg usage without macro collision.
    
    This fixes velinux 5.15.198+ while keeping existing behavior on other kernels.
    
    Reviewed-by: S A Muqthyar Ahmed <syed.abdul.muqthyar.ahmed@intel.com>
    Signed-off-by: Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>

--- backport/backport-include/linux/slab.h
+++ backport/backport-include/linux/slab.h
@@ -174,12 +174,19 @@
 
 #ifdef BPM_KVREALLOC_OLDSIZE_PARAM_PRESENT
 #ifndef BPM_KVREALLOC_NOPROF_PRESENT
-#include <linux/mm.h>
-static inline void *__bp_kvrealloc(const void *p, size_t newsize, gfp_t flags)
-{
-	return kvrealloc(p, 0, newsize, flags);
-}
-#define kvrealloc(p, s, f) __bp_kvrealloc(p, s, f)
+
+#define __bp_kvrealloc_3(_p, _newsize, _flags) \
+	kvrealloc(_p, 0, _newsize, _flags)
+#define __bp_kvrealloc_4(_p, _oldsize, _newsize, _flags) \
+	kvrealloc(_p, _oldsize, _newsize, _flags)
+#define __bp_kvrealloc_pick(_1, _2, _3, _4, _pick, ...) _pick
+
+#ifndef kvrealloc
+#define kvrealloc(...) \
+	__bp_kvrealloc_pick(__VA_ARGS__, __bp_kvrealloc_4, __bp_kvrealloc_3) \
+	(__VA_ARGS__)
+#endif
+
 #endif
 #endif
 

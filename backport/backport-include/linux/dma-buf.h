5ecb9b92101a8da6e8bdb573a0b37cd503a70d4f
Akanksha Hotta <akanksha.hotta@intel.com>
2026-07-03 15:30:09 +0530

    backport-include/dma-buf: backport dma_buf_invalidate_mappings()
    error:
    -------------------------------------------------------------------------------
    implicit declaration of function 'dma_buf_invalidate_mappings'
    -------------------------------------------------------------------------------
    
    Reference:
       95308225e5ba
       dma-buf: Rename dma_buf_move_notify() to dma_buf_invalidate_mappings()
    
    Reviewed-by: Ayaz A Siddiqui <ayaz.siddiqui@intel.com>
    Reviewed-by: Syed Abdul Muqthyar Ahmed <syed.abdul.muqthyar.ahmed@intel.com>
    Signed-off-by: Pravalika Gurram <pravalika.gurram@intel.com>

--- backport/backport-include/linux/dma-buf.h
+++ backport/backport-include/linux/dma-buf.h
@@ -21,4 +21,11 @@ MODULE_IMPORT_NS(DMA_BUF);
 MODULE_IMPORT_NS("DMA_BUF");
 #endif
 
+#ifdef BPM_DMA_BUF_INVALIDATE_MAPPINGS_NOT_PRESENT
+static inline void dma_buf_invalidate_mappings(struct dma_buf *dma_buf)
+{
+    dma_buf_move_notify(dma_buf);
+}
+#endif /* BPM_DMA_BUF_INVALIDATE_MAPPINGS_NOT_PRESENT */
+
 #endif /*__BACKPORT_DMA_BUF_H__*/

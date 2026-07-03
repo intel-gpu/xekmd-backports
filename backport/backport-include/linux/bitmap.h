4271ae5363d1b55303febaf66d623936f7198f23
Akanksha Hotta <akanksha.hotta@intel.com>
2026-07-03 15:05:38 +0530

    backport-include/bitmap: backport bitmap_weighted_or()
    error:
    -------------------------------------------------------------------------------
    implicit declaration of function 'bitmap_weighted_or'
      [-Werror=implicit-function-declaration]
    -------------------------------------------------------------------------------
    
    Reference:
       437cb3ded250
       cpumask: Introduce cpumask_weighted_or()
    
    Reviewed-by: Ayaz A Siddiqui <ayaz.siddiqui@intel.com>
    Reviewed-by: Syed Abdul Muqthyar Ahmed <syed.abdul.muqthyar.ahmed@intel.com>
    Signed-off-by: Pravalika Gurram <pravalika.gurram@intel.com>
    Signed-off-by: Akanksha Hotta <akanksha.hotta@intel.com>
    (cherry picked from commit 2d900bd41fc05e69e144f9c2586a0f4ddea63488)

---
+++ backport/backport-include/linux/bitmap.h
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _BACKPORT_LINUX_BITMAP_H
+#define _BACKPORT_LINUX_BITMAP_H
+#include_next <linux/bitmap.h>
+
+#ifdef BPM_BITMAP_WEIGHTED_OR_NOT_PRESENT
+static __always_inline
+unsigned int bitmap_weighted_or(unsigned long *dst, const unsigned long *src1,
+				const unsigned long *src2, unsigned int nbits)
+{
+	if (small_const_nbits(nbits)) {
+		*dst = *src1 | *src2;
+		return hweight_long(*dst & BITMAP_LAST_WORD_MASK(nbits));
+	} else {
+		return __bitmap_weighted_or(dst, src1, src2, nbits);
+	}
+}
+#endif /* BPM_BITMAP_WEIGHTED_OR_NOT_PRESENTT */
+
+#endif /* _BACKPORT_LINUX_BITMAP_H */

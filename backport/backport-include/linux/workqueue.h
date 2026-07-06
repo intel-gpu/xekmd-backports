bded4cdcbea9decbed834d61a49b70df5dc7164c
Akanksha Hotta <akanksha.hotta@intel.com>
2026-07-06 15:19:28 +0530

    backport-include/linux/workqueue: provide fallback for system_dfl_wq() unavailability
    The reason of introducing system_dfl_wq() is to encourage this workqueue
    to be used as a default workqueue in place of or alongside
    system_unbound_wq().
    
    So wherever, system_dfl_wq() is not available fallback to system_unbound_wq().
    
    error:
    -------------------------------------------------------------------------------
    error: 'system_dfl_wq' undeclared (first use in this function);
           did you mean 'system_unbound_wq'?
    -------------------------------------------------------------------------------
    
    Reference:
       128ea9f6ccfb
       workqueue: Add system_percpu_wq and system_dfl_wq
    
    Reviewed-by: Ayaz A Siddiqui <ayaz.siddiqui@intel.com>
    Reviewed-by: Syed Abdul Muqthyar Ahmed <syed.abdul.muqthyar.ahmed@intel.com>
    Signed-off-by: Pravalika Gurram <pravalika.gurram@intel.com>

--- backport/backport-include/linux/workqueue.h
+++ backport/backport-include/linux/workqueue.h
@@ -17,6 +17,8 @@
 #define disable_work_sync cancel_work_sync
 #endif
 
+#define system_dfl_wq system_unbound_wq
+
 #ifdef BPM_SYSTEM_PERCPU_WQ_NOT_PRESENT
 #define system_percpu_wq system_wq
 #endif

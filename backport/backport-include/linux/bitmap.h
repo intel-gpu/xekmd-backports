f87ca8125f6f4febd39498486ff577c49d34eadc
Akanksha Hotta <akanksha.hotta@intel.com>
2026-07-06 22:25:13 +0530

    bitmap: Provide bitmap_weighted_or compatibility for 7.1KV
    In 7.1 kernel code bitmap_weighted_or() to combine two bitmaps and return
    the resulting weight in a single pass. So when the target is compiled
    against ubuntu_7.0, below error is being observed.
    
    error:
    -------------------------------------------------------------------------------
    ERROR: modpost: "__bitmap_weighted_or" [drivers/gpu/drm/xe/xe.ko] undefined!
    -------------------------------------------------------------------------------
    
    Therefore created a compatibility implementation in backport include bitmap
    header:
       - perform bitmap_or(dst, src1, src2, nbits)
       - return bitmap_weight(dst, nbits)
    
    Signed-off-by: Akanksha Hotta <akanksha.hotta@intel.com>

--- backport/backport-include/linux/bitmap.h
+++ backport/backport-include/linux/bitmap.h
@@ -3,13 +3,7 @@
 #define _BACKPORT_LINUX_BITMAP_H
 #include_next <linux/bitmap.h>
 
-#ifdef BPM_BITMAP_WEIGHTED_OR_NOT_PRESENT
-/* bitmap_weighted_or - return the weight of the OR of two bitmaps
- * Similar to bitmap_or() but returns the weight (number of bits set)
- * instead of just performing the operation.
- * This function performs: dst = src1 | src2 and returns popcount(dst)
- */
-static inline unsigned int bitmap_weighted_or(unsigned long *dst,
+static inline unsigned int __backport_bitmap_weighted_or(unsigned long *dst,
 		const unsigned long *src1,
 		const unsigned long *src2,
 		unsigned int nbits)
@@ -17,7 +11,10 @@ static inline unsigned int bitmap_weighted_or(unsigned long *dst,
 	bitmap_or(dst, src1, src2, nbits);
 	return bitmap_weight(dst, nbits);
 }
-#endif /* BPM_BITMAP_WEIGHTED_OR_NOT_PRESENT */
+
+#undef bitmap_weighted_or
+#define bitmap_weighted_or(dst, src1, src2, nbits) \
+	__backport_bitmap_weighted_or((dst), (src1), (src2), (nbits))
 
 #endif /* _BACKPORT_LINUX_BITMAP_H */
 

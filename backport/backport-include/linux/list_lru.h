6967a7423a462f00aed9ef4bac1236c0dc68fae4
Akanksha Hotta <akanksha.hotta@intel.com>
2026-07-06 10:55:16 +0530

    backport-include/linux/list_lru: backport list_lru_add()
    For the kernels exposed list_lru_add(lru, item) 2-argument form, nid and
    memcg arguements are extracted from the list head and lru arguments
    themselves.
    
    Create a macro to replace 4-arguments to 2-arguments to avoid arguement mismatches.
    Since data is extracted from 2-arguments so there is no functional
    impact.
    
    error:
    -----------------------------------------------------------
    error: too many arguments to function 'list_lru_add'
    -----------------------------------------------------------
    
    Reference:
       0a97c01cd20b
       list_lru: allow explicit memcg and NUMA node selection
    
    Reviewed-by: Ayaz A Siddiqui <ayaz.siddiqui@intel.com>
    Reviewed-by: Syed Abdul Muqthyar Ahmed <syed.abdul.muqthyar.ahmed@intel.com>
    Signed-off-by: Pravalika Gurram <pravalika.gurram@intel.com>

---
+++ backport/backport-include/linux/list_lru.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef __BACKPORT_LIST_LRU_H__
+#define __BACKPORT_LIST_LRU_H__
+
+#include <linux/version.h>
+#include_next <linux/list_lru.h>
+
+#ifdef BPM_LIST_LRU_ADD_4ARGS_NOT_PRESENT
+#define list_lru_add(lru, item, nid, memcg) list_lru_add(lru, item)
+#endif
+
+#endif /* __BACKPORT_LIST_LRU_H__ */

5bacdfd6927e3bb6567e79ed7010384749dca289
Rachapalli Bandi Jagadeesh <jagadeesh.rachapalli.bandi@intel.com>
2026-07-08 16:26:46 +0530

    Add shrinker.h compat for register_shrinker() arg count
    Newer callsites pass an optional format/name argument to
    register_shrinker(), which legacy kernels don't support. Add a
    backport-include wrapper for linux/shrinker.h that, when
    BPM_REGISTER_SHRINKER_ARG2_NOT_PRESENT is defined, drops the extra
    variadic arguments so the driver builds against the single-argument
    register_shrinker() API.
    Also move patch changes into backport header file.
    
    Reviewed-by: Ayaz A Siddiqui <ayaz.siddiqui@intel.com>
    Signed-off-by: Rachapalli Bandi Jagadeesh <jagadeesh.rachapalli.bandi@intel.com>

--- backport/backport-include/linux/shrinker.h
+++ backport/backport-include/linux/shrinker.h
@@ -8,4 +8,12 @@
 
 #include_next <linux/shrinker.h>
 
+#ifdef BPM_REGISTER_SHRINKER_ARG2_NOT_PRESENT
+/*
+ * Legacy kernels expose register_shrinker(struct shrinker *) only.
+ * Drop optional format/name arguments used by newer callsites.
+ */
+#define register_shrinker(shrinker, fmt, ...) register_shrinker(shrinker)
+#endif
+
 #endif /* __BACKPORT_LINUX_SHRINKER_H__ */

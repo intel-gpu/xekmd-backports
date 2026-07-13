a1bde525ed90dfda59b6674ddccdaae47a3ebc79
Akanksha Hotta <akanksha.hotta@intel.com>
2026-07-13 19:38:54 +0530

    compat/bitmap: backport missing export symbol  __bitmap_weighted_or()
    error:
    -------------------------------------------------------------------------------
    modpost: "__bitmap_weighted_or" [drivers/gpu/drm/xe/xe.ko] undefined!
    -------------------------------------------------------------------------------
    
    Reference:
       95d324fb1b
       bitmap: add test_zero_nbits()
    
    Reviewed-by: Ayaz A Siddiqui <ayaz.siddiqui@intel.com>
    Reviewed-by: Syed Abdul Muqthyar Ahmed <syed.abdul.muqthyar.ahmed@intel.com>
    Signed-off-by: Akanksha Hotta <akanksha.hotta@intel.com>
    (cherry picked from commit ab7d9553e37d00ee129b139771cc172b2e30da88)

--- backport/backport-include/linux/bitmap.h
+++ backport/backport-include/linux/bitmap.h
@@ -3,6 +3,11 @@
 #define _BACKPORT_LINUX_BITMAP_H
 #include_next <linux/bitmap.h>
 
+#ifdef BPM_UNDERSCORE_BITMAP_WEIGHTED_OR_NOT_PRESENT
+unsigned int __bitmap_weighted_or(unsigned long *dst, const unsigned long *bitmap1,
+				  const unsigned long *bitmap2, unsigned int nbits);
+#endif /* BPM_UNDERSCORE_BITMAP_WEIGHTED_OR_NOT_PRESENT */
+
 #ifdef BPM_BITMAP_WEIGHTED_OR_NOT_PRESENT
 static __always_inline
 unsigned int bitmap_weighted_or(unsigned long *dst, const unsigned long *src1,

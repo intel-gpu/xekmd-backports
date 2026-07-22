intel-gpu/xekmd-backports

commit 974386bf1c5e5cf298ccf74f46a0b1cff8f7822a

backport-include/slab.h: fix kvrealloc compat on 5.15.198 headers
The old kvrealloc compat used a fixed 3-arg macro, which can break header
parsing order on velinux 5.15.198 headers and causes below errors.

Error:
--------------------------------------------------------------------------------
implicit declaration of function ‘kvrealloc’
--------------------------------------------------------------------------------

Use a safer header-only kvrealloc compat form that handles both 3-arg
and 4-arg usage without macro collision.

This fixes velinux 5.15.198+ while keeping existing behavior on other kernels.

Reviewed-by: S A Muqthyar Ahmed <syed.abdul.muqthyar.ahmed@intel.com>
Signed-off-by: Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>

backport/backport-include/linux/slab.h
@@ -7,12 +7,19 @@
 
 #ifdef BPM_KVREALLOC_OLDSIZE_PARAM_PRESENT
 #ifndef BPM_KVREALLOC_NOPROF_PRESENT
-#include <linux/mm.h>
-static inline void *__bp_kvrealloc(const void *p, size_t newsize, gfp_t flags)
-{
-	return kvrealloc(p, 0, newsize, flags);
-}
-#define kvrealloc(p, s, f) __bp_kvrealloc(p, s, f)
+
+#define __bp_kvrealloc_3(_p, _newsize, _flags) \
+	kvrealloc(_p, 0, _newsize, _flags)
+#define __bp_kvrealloc_4(_p, _oldsize, _newsize, _flags) \
+	kvrealloc(_p, _oldsize, _newsize, _flags)
+#define __bp_kvrealloc_pick(_1, _2, _3, _4, _pick, ...) _pick
+
+#ifndef kvrealloc
+#define kvrealloc(...) \
+	__bp_kvrealloc_pick(__VA_ARGS__, __bp_kvrealloc_4, __bp_kvrealloc_3) \
+	(__VA_ARGS__)
+#endif
+
 #endif
 #endif
 

commit 01333900a10ed6a53659a759e83ae8a7b09d5033

linux/slab: Fix kvrealloc() compatibility wrapper
The backport kvrealloc() wrapper passed an old size of 0 for
three-argument kvrealloc() calls. As a result, existing allocation
contents were not preserved when the buffer was resized.

Use ksize() to determine the current allocation size before calling
kvrealloc(), ensuring existing data is retained on older kernels.

Error:
------------------------------------------------------------------------------
[  657.915217] BUG: unable to handle page fault for address: 00000000000011d4
[  657.923414] #PF: supervisor read access in kernel mode
[  657.929873] #PF: error_code(0x0000) - not-present page
[  657.936328] PGD 0 P4D 0
	       ------
[  657.970703] Workqueue: xe-ordered-wq preempt_rebind_work_func [xe]
[  657.978271] RIP: 0010:xe_vm_validate_rebind+0x70/0xa0 [xe]
------------------------------------------------------------------------------

Fixes: 974386b ("backport-include/slab.h: fix kvrealloc compat on 5.15.198 headers")

Signed-off-by: Bommu Krishnaiah <krishnaiah.bommu@intel.com>

backport/backport-include/linux/slab.h
@@ -9,7 +9,7 @@
 #ifndef BPM_KVREALLOC_NOPROF_PRESENT
 
 #define __bp_kvrealloc_3(_p, _newsize, _flags) \
-	kvrealloc(_p, 0, _newsize, _flags)
+	kvrealloc(_p, ksize(_p), _newsize, _flags)
 #define __bp_kvrealloc_4(_p, _oldsize, _newsize, _flags) \
 	kvrealloc(_p, _oldsize, _newsize, _flags)
 #define __bp_kvrealloc_pick(_1, _2, _3, _4, _pick, ...) _pick
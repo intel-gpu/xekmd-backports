ec84ff1d7ec0521fdd70c4ccde63bc4e6c55436f
Bommu Krishnaiah <krishnaiah.bommu@intel.com>
2026-07-22 07:17:10 +0530

    linux/slab: Fix kvrealloc() compatibility wrapper
    The backport kvrealloc() wrapper passed an old size of 0 for
    three-argument kvrealloc() calls. As a result, existing allocation
    contents were not preserved when the buffer was resized.
    
    Use ksize() to determine the current allocation size before calling
    kvrealloc(), ensuring existing data is retained on older kernels.
    
    Error:
    ------------------------------------------------------------------------------
    [  657.915217] BUG: unable to handle page fault for address: 00000000000011d4
    [  657.923414] #PF: supervisor read access in kernel mode
    [  657.929873] #PF: error_code(0x0000) - not-present page
    [  657.936328] PGD 0 P4D 0
    	       ------
    [  657.970703] Workqueue: xe-ordered-wq preempt_rebind_work_func [xe]
    [  657.978271] RIP: 0010:xe_vm_validate_rebind+0x70/0xa0 [xe]
    ------------------------------------------------------------------------------
    
    Fixes: 974386b ("backport-include/slab.h: fix kvrealloc compat on 5.15.198 headers")
    
    Signed-off-by: Bommu Krishnaiah <krishnaiah.bommu@intel.com>

--- backport/backport-include/linux/slab.h
+++ backport/backport-include/linux/slab.h
@@ -9,7 +9,7 @@
 #ifndef BPM_KVREALLOC_NOPROF_PRESENT
 
 #define __bp_kvrealloc_3(_p, _newsize, _flags) \
-	kvrealloc(_p, 0, _newsize, _flags)
+	kvrealloc(_p, ksize(_p), _newsize, _flags)
 #define __bp_kvrealloc_4(_p, _oldsize, _newsize, _flags) \
 	kvrealloc(_p, _oldsize, _newsize, _flags)
 #define __bp_kvrealloc_pick(_1, _2, _3, _4, _pick, ...) _pick

a79419aca2a980a23fdc27b87c9ca3909488df49
Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>
2026-08-11 11:56:51 +0530

    backport-include/mei: add uuid_le_cmp fallback in mei_uuid.h
    On some newer-kernel paths, MEI users see uuid_le but not uuid_le_cmp, causing
    implicit declaration errors. Add a probe-guarded fallback in
    backport-include/linux/mei_uuid.h so all MEI files get a consistent
    helper via the common include path.
    
    error:
    implicit declaration of function uuid_le_cmp
    
    Reference:
       1fb1ea0d9cb8 (mei: Move uuid.h to the MEI namespace)
    
    Reviewed-by: Syed Abdul Muqthyar Ahmed <syed.abdul.muqthyar.ahmed@intel.com>
    Signed-off-by: Kanaka Raju Nayana <kanaka.raju.nayana@intel.com>

--- backport/backport-include/linux/mei_uuid.h
+++ backport/backport-include/linux/mei_uuid.h
@@ -8,4 +8,10 @@
 #include_next <linux/mei_uuid.h>
 #endif
 
+#ifdef BPM_UUID_LE_CMP_NOT_PRESENT
+#ifndef uuid_le_cmp
+#define uuid_le_cmp(u1, u2) memcmp(&(u1), &(u2), sizeof(uuid_le))
+#endif
+#endif
+
 #endif /* _BACKPORT_LINUX_MEI_UUID_H */

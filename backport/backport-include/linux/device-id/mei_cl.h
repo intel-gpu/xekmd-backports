e14c31374dbc4fc4970db1a6b81d0c19f85e5897
Rachapalli Bandi Jagadeesh <jagadeesh.rachapalli.bandi@intel.com>
2026-08-27 13:31:13 +0530

    mei_cl.h: backport device-id/mei_cl.h header file
    mod_devicetable.h header split into per subsystem headers like mei_cl.h
    and split MEI_CL_* and struct mei_cl_device_id out of it into a new
    <linux/device-id/mei_cl.h>, and <linux/mei_cl_bus.h>
    
    fatal error:
    ----------------------------------------------------------------------------
    linux/mei_cl_bus.h:10: linux/device-id/mei_cl.h: No such file or directory
    ----------------------------------------------------------------------------
    
    Copying the upstream header into copy-list will cause redefinition errors
    because older kernel still carries the same macros & struct in mod_devicetable.h
    
    To fix both the errors, add a tny shim under mei_cl.h header that
    just pulls in <linux/mod_devicetable.h> and plus the __KERNEL bits from upstream
    that satisfies the include without duplicating any definitions older kernel
    already provide.
    
    Reference:
    	ad428f5811bd mod_devicetable.h: Split into per subsystem headers
    
    Signed-off-by: Rachapalli Bandi Jagadeesh <jagadeesh.rachapalli.bandi@intel.com>

---
+++ backport/backport-include/linux/device-id/mei_cl.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef BPM_DEVICE_ID_MEI_CL_H
+#define BPM_DEVICE_ID_MEI_CL_H
+
+#ifdef __KERNEL__
+#include <linux/types.h>
+#include <linux/mei_uuid.h>
+typedef unsigned long kernel_ulong_t;
+#endif
+
+#include <linux/mod_devicetable.h>
+
+#endif /* BPM_DEVICE_ID_MEI_CL_H */
